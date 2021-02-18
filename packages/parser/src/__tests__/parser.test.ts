291af99fe2bc30bbd114ed82787e9dbff4d17e1a
Alex Khizhnyi <admin@devblog.pro>
2021-02-18 17:21:22 +0200

    fix test

--- packages/parser/src/__tests__/parser.test.ts
+++ packages/parser/src/__tests__/parser.test.ts
@@ -1,64 +1,44 @@
-import {readdirSync, readFileSync, unlinkSync, writeFileSync} from 'fs';
+import {readdirSync, readFileSync} from 'fs';
 import {join} from 'path';
 import {parser} from '../parser';
 
 const successPath = 'success';
 const errorPath = 'error';
 
-const readFile = (name: string, folder = successPath) => {
-    return readFileSync(join(__dirname, folder, name), 'utf-8');
-};
-
-const writeFile = (name: string, body: string, folder = successPath) => {
-    return writeFileSync(join(__dirname, folder, name), body, 'utf-8');
-};
-
-const readJSON = (file: string, folder = successPath) => {
-    return JSON.parse(readFileSync(join(__dirname, folder, `${file.replace('.proto', '.json')}`), 'utf8'), (_, i: unknown) => {
-        return (i === null ? undefined : i);
-    }) as Record<string, unknown>;
-};
-
-const deleteFile = (name: string, folder = successPath) => {
-    return unlinkSync(join(__dirname, folder, name));
-};
-const filterProto = (file: string) => {
-    return /\.proto$/.test(file);
-};
-
-const successFiles = readdirSync(join(__dirname, successPath));
-const errorFiles = readdirSync(join(__dirname, errorPath));
-
 describe('parser', () => {
     describe(successPath, () => {
-        successFiles.filter(filterProto).forEach((file) => {
+        readdirSync(join(__dirname, successPath)).filter(file => {
+            return /\.proto$/.test(file);
+        }).forEach((file) => {
             test(file, () => {
-                const parsed = parser(readFile(file));
-                const fName = file.replace('.proto', '.diff.json');
+                const content = readFileSync(join(__dirname, successPath, file), 'utf-8');
+                const parsed = parser(content);
 
-                writeFile(fName, JSON.stringify(parsed, null, 2));
-                expect(parsed).toEqual(readJSON(file));
-                deleteFile(fName);
+                const json = JSON.stringify(parsed, null, 2);
+
+                const expected = readFileSync(join(__dirname, successPath, `${file.replace('.proto', '.json')}`), 'utf8');
+
+                expect(json).toEqual(JSON.parse(expected));
             });
         });
     });
 
     describe(errorPath, () => {
-        errorFiles.filter(filterProto).forEach((file) => {
+        readdirSync(join(__dirname, errorPath)).filter(file => {
+            return /\.proto$/.test(file);
+        }).forEach((file) => {
             test(file, () => {
-                let error = false;
-                const fName = file.replace('.proto', '.diff.json');
+                expect.assertions(1);
+
+                const content = readFileSync(join(__dirname, errorPath, file), 'utf-8');
 
                 try {
-                    parser(readFile(file, errorPath));
-                } catch (e) {
-                    error = true;
-                    writeFile(fName, JSON.stringify(e, null, 2), errorPath);
-                    expect(e).toEqual(readJSON(file, errorPath));
-                    deleteFile(fName, errorPath);
-                }
+                    parser(content);
+                } catch (error) {
+                    const expected = readFileSync(join(__dirname, errorPath, `${file.replace('.proto', '.json')}`), 'utf8');
 
-                expect(error).toBe(true);
+                    expect(error).toEqual(expected);
+                }
             });
         });
     });
